949360888789c0468b218b7d2c6193859a3fcc09
Rinci <rinciatrey@gmail.com>
2025-11-21 22:18:46 +0530

    Frontend form

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+import { Box, Container, TextField, Typography } from '@mui/material'
+
+function App() {
+  const [emailContent, setEmailContent] = useState('')
+  const [tone, setTone] = useState('')
+  const [generatedReply, setGeneratedReply] = useState('') 
+  const [loading, setLoading] = useState(false) 
+  const [error, setError] = useState('') 
+
+  return (
+    <>
+      <Container maxWidth="md" sx={{py:4}}>
+            <Typography variant='h3' component="h1" gutterBottom>
+              Email Reply generator
+            </Typography>
+            <Box sx={{mx:3}}>
+          <TextField
+          fullWidth
+          multiline
+          rows={6}
+          variant='outlined'
+          label="Original Email Content"
+          value={emailContent || ''}
+          onChange={(e)=> setEmailContent(e.target.value)}
+          sx={{mb:2}}
+               />
+            </Box>
+      </Container>
+    </>
+  )
+}
+
+export default App
